eacd940f316388ebdeabcbcbec91794a9c9c20ec
bilibilihuazi <2825338723@qq.com>
2025-02-14 19:45:44 +0800

    SourceCode

---
+++ Main_Window.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace AutoUpdateTest
+{
+    partial class Main_Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 249);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Main_Window";
+            this.Text = "Main_Window";
+            this.Load += new System.EventHandler(this.Main_Window_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Main_Window.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoUpdateTest
+{
+    public partial class Main_Window: Form
+    {
+        //变量========================================================================================
+        public static string Title = "自动更新测试程序,当前版本:";
+        public static string Version = "Release1.0.0.0";
+        //函数========================================================================================
+
+        //检查更新
+        public static void CheckUpdate()
+        {
+
+        }
+        //构造函数====================================================================================
+        public Main_Window()
+        {
+            InitializeComponent();
+
+        }
+        //主代码区======================================================================================
+        private void Main_Window_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
